b49ea195c7702a4191bcca05993cf292bc2c906b
guomingyang <myguo@aibee.com>
2020-08-31 10:46:16 +0800

    fix enter keypress case

--- src/views/admin/login.vue
+++ src/views/admin/login.vue
@@ -5,7 +5,7 @@
         <el-input size="mini" v-model="user.username"></el-input>
       </el-form-item>
       <el-form-item label="Password: ">
-        <el-input size="mini" v-model="user.password" type="password" @keypress.native="handleLogin"></el-input>
+        <el-input size="mini" v-model="user.password" type="password" @keypress.native="handleKeyPress"></el-input>
       </el-form-item>
     </el-form>
     <el-button size="small" @click="handleLogin">登 陆</el-button>
@@ -24,6 +24,12 @@
       }
     },
     methods: {
+      handleKeyPress(e) {
+        switch (e.keyCode) {
+          case 13:
+            this.handleLogin()
+        }
+      },
       handleLogin() {
         deleteAuthCookie()
         auth(this.user).then(() => {
